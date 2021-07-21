d0e10d477018e783858a7999324f57fac028f47e
BetterAB <87430564+BetterAB@users.noreply.github.com>
2021-07-21 23:25:50 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,1 +1,11 @@
-
+document.addEventListener("DOMContentLoaded",function (event) {
+      let targetImage = document.querySelector("#smart-image");
+	  targetImage.classList.add("small");
+		  targetImage.addEventListener("click",function(){
+		  		  targetImage.classList.remove("small");
+		  })
+		
+		  
+	  
+	 
+})
